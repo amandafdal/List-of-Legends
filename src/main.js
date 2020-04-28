e073a478e6a64c6efac24239d41db8d6c655d294
amandafdal <amandafdal@gmail.com>
2020-04-28 17:48:02 -0300

    display dos personagens na tela

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-
+import data from './data/lol/lol.js';
 console.log(example, data);
+
+const cardsField = document.getElementById("champ-box") 
+const lol = data.data
+
+function createCards(listdata){
+    for(let champ in listdata){
+        const card = document.createElement("div");
+        card.className = "champ-card";
+        card.id = `${lol[champ].id}`;
+        card.innerHTML = 
+        `<img class="champ-img" id=${lol[champ].name} src=${lol[champ].img} alt=${lol[champ].name}><br>
+        <p class="champ-name">${lol[champ].name}</p>`;
+        cardsField.appendChild(card) 
+    };
+};
+createCards(lol);
