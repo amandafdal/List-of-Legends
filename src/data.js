0d62a076f792e5a703e1b11e9f529810231e6e4b
amandafdal <amandafdal@gmail.com>
2020-05-09 18:19:05 -0300

    refatoração das fun~ções de filtro por role e dificuldade

--- src/data.js
+++ src/data.js
@@ -1,10 +1,16 @@
-// funções de exemplo
-
-export const example = () => {
-  return 'example';
+export const filterRole = (data, roleValue) => {
+  return data.filter(role => role.tags.includes(roleValue));
 };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
 
+export const filterDifficulty = {
+  easy:(data, diffValue) => {
+    return data.filter(diff => diff.info.difficulty <= diffValue)
+  },
+  medium:(data, diffValue1, diffValue2) => {
+    return data.filter(diff => (diff.info.difficulty >diffValue1) && (diff.info.difficulty <=diffValue2))
+  },
+  hard:(data, diffValue) => {
+    return data.filter(diff => diff.info.difficulty >= diffValue)
+  }
+};
